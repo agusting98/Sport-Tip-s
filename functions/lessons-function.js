913d1ae85fc1cddf8f4ad631cec9e9de561d4767
agusting98 <agustinnicolasgallardor@gmail.com>
2024-10-06 19:01:27 -0300

    creacion de repositorio sport fit's

---
+++ functions/lessons-function.js
@@ -0,0 +1,99 @@
+document.getElementById('toggleButton_1').addEventListener('click', function() {
+    const textSection_unit1 = document.getElementById('textSection_unit1');
+    
+    if (textSection_unit1.style.display === 'none' || textSection_unit1.style.display === '') {
+        textSection_unit1.style.display = 'block';
+    } else {
+        textSection_unit1.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_2').addEventListener('click', function() {
+    const textSection_unit2 = document.getElementById('textSection_unit2');
+    
+    if (textSection_unit2.style.display === 'none' || textSection_unit2.style.display === '') {
+        textSection_unit2.style.display = 'block';
+    } else {
+        textSection_unit2.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_3').addEventListener('click', function() {
+    const textSection_unit3 = document.getElementById('textSection_unit3');
+    
+    if (textSection_unit3.style.display === 'none' || textSection_unit3.style.display === '') {
+        textSection_unit3.style.display = 'block';
+    } else {
+        textSection_unit3.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_4').addEventListener('click', function() {
+    const textSection_unit4 = document.getElementById('textSection_unit4');
+    
+    if (textSection_unit4.style.display === 'none' || textSection_unit4.style.display === '') {
+        textSection_unit4.style.display = 'block';
+    } else {
+        textSection_unit4.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_5').addEventListener('click', function() {
+    const textSection_unit5 = document.getElementById('textSection_unit5');
+    
+    if (textSection_unit5.style.display === 'none' || textSection_unit5.style.display === '') {
+        textSection_unit5.style.display = 'block';
+    } else {
+        textSection_unit5.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_6').addEventListener('click', function() {
+    const textSection_unit6 = document.getElementById('textSection_unit6');
+    
+    if (textSection_unit6.style.display === 'none' || textSection_unit6.style.display === '') {
+        textSection_unit6.style.display = 'block';
+    } else {
+        textSection_unit6.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_7').addEventListener('click', function() {
+    const textSection_unit7 = document.getElementById('textSection_unit7');
+    
+    if (textSection_unit7.style.display === 'none' || textSection_unit7.style.display === '') {
+        textSection_unit7.style.display = 'block';
+    } else {
+        textSection_unit7.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_8').addEventListener('click', function() {
+    const textSection_unit8 = document.getElementById('textSection_unit8');
+    
+    if (textSection_unit8.style.display === 'none' || textSection_unit8.style.display === '') {
+        textSection_unit8.style.display = 'block';
+    } else {
+        textSection_unit8.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_9').addEventListener('click', function() {
+    const textSection_unit9 = document.getElementById('textSection_unit9');
+    
+    if (textSection_unit9.style.display === 'none' || textSection_unit9.style.display === '') {
+        textSection_unit9.style.display = 'block';
+    } else {
+        textSection_unit9.style.display = 'none';
+    }
+});
+
+document.getElementById('toggleButton_10').addEventListener('click', function() {
+    const textSection_unit10 = document.getElementById('textSection_unit10');
+    
+    if (textSection_unit10.style.display === 'none' || textSection_unit10.style.display === '') {
+        textSection_unit10.style.display = 'block';
+    } else {
+        textSection_unit10.style.display = 'none';
+    }
+});
